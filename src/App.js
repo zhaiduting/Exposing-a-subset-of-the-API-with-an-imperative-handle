3f64c659defde0610a5d04ebe984f04ee8a9265d
zhaiduting <zhaiduting@163.com>
2023-05-30 03:10:12 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import { forwardRef, useRef, useImperativeHandle } from "react";
 
 const MyInput = forwardRef((props, ref) => {
   const realInputRef = useRef(null);
+  
+  // 将 <input> 节点赋给 realInputRef.current
+  const jsx = (
+    <input {...props} ref={realInputRef} value="Click it several times!" />
+  );
+  
+  // 执行函数 useImperativeHandle(ref, callback) 
+  // 执行的结果：将 callback() 返回的自定义对象 {focus: function focus(){...}} 赋给了 ref.current
   useImperativeHandle(ref, () => ({
     focus() {
       realInputRef.current.style.color = ["green", "red", "blue", "pink"][
@@ -10,9 +18,11 @@ const MyInput = forwardRef((props, ref) => {
       realInputRef.current.focus();
     }
   }));
-  const jsx = (
-    <input {...props} ref={realInputRef} value="Click it several times!" />
-  );
+  
+  // 当父组件调用 ref.current.focus() 方法时，
+  // 实际调用的是自定义对象 {focus(){..}} 的 focus() 方法
+  // 在 focus(){..} 方法内部对 realInputRef.current 进行了相关操作 
+  // 操作 realInputRef.current 其实就是操作 DOM 节点 <input>
   return jsx;
 });
 
